1c567c9e5f58751aef4fa8814312d756372b3848
Matthew Paul <mpaul0416@gmail.com>
2013-10-30 20:34:52 -0400

    Initial commit for name builder

---
+++ Name Lich/NameBuilder/NameBuilder.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Name_Lich.NameBuilder
+{
+    public partial class NameBuilder : Form
+    {
+        public NameBuilder()
+        {
+            InitializeComponent();
+        }
+    }
+}
